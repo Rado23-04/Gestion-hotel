81ec71b663954369ab5ba11f591f0e1188bc4124
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-07-15 02:39:42 +0300

    feat: update migration3.sql

--- implementation/migration/migration3.sql
+++ implementation/migration/migration3.sql
@@ -3,23 +3,31 @@ DROP TABLE cancel;
 DROP TABLE have;
 DROP TABLE choose;
 
---creer les tables receive, room_type, influence et affiliate
-CREATE TABLE receive ( -- à creer
+--créer les tables receive, room_type, influence et affiliate
+CREATE TABLE receive ( 
     id_employee int references receptionist(id),
     id_payment int references payment(id)
 );
 
-CREATE TABLE room_type(  -- à creer
+CREATE TABLE room_type(  
     id serial PRIMARY KEY,
     "name" Varchar(100) NOT NULL
 );
 
-CREATE TABLE influence( --à creer
+CREATE TABLE influence(
     id_price int references price(id),
     id_season int references Season(id)
 );
 
-CREATE TABLE affiliate( -- à creer
+CREATE TABLE affiliate( 
     id_promotion int references promotion(id),
     id_room int references room(id)
 );
+
+--Supprimer la colonne "id_hotel":
+ALTER TABLE province_available
+DROP COLUMN id_hotel CASCADE;
+
+--Ajouter une nouvelle colonne "id_hotel":
+ALTER TABLE receptionist
+ADD COLUMN id_hotel int references hotel(id);
