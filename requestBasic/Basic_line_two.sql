7f746c32223d71e4580dcb9dd31924679aecf748
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-07-14 21:24:14 +0300

    feat: Basic_line_two.sql

--- requestBasic/Basic_line_two.sql
+++ requestBasic/Basic_line_two.sql
@@ -1,1 +1,6 @@
--- Afficher la liste des réservations d'un client donné
+-- Afficher la liste des clients qui n'ont pas encore payé en totalité leurs frais
+
+SELECT *
+FROM customer c
+JOIN payment p ON c.id = p.id_customer
+WHERE p.total_amount_status = false;
