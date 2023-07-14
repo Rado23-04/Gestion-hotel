217aab64741e1bbe4934e61750354169134b13ff
Fifaliana <hei.fifaliana@gmail.com>
2023-07-14 16:42:38 +0300

    fix : request room type

--- requestBasic/basic_line_three.sql
+++ requestBasic/basic_line_three.sql
@@ -1,10 +1,10 @@
 -- Afficher une liste des rooms d'un type donné (chambre simple, twin, vip...), dans un hotel donné 
 -- Cette requête montre type de room dans un hotel 
-SELECT room.id_room, room.number, room_type.name, hotel.hotel_name
+SELECT room.id, room.number, room_type.name AS room_type, hotel.name AS hotel_name
 FROM room
-JOIN hotel ON room.id_hotel = hotel.id_hotel
-JOIN room_type ON room.id = room_type.id
+JOIN hotel ON room.id_hotel = hotel.id
+JOIN room_type ON room.id_room_type = room_type.id
 WHERE room_type.name = 'Vip'
-  AND hotel.hotel_name = 'Akory' ;
-  
+  AND hotel.name = 'Akory A';
+
   
