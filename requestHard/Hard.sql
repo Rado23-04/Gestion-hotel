3ac8b6ec1a83fa38926f85414d1bd8f7df7eb7bc
mampiononavoahary <hei.zoarisoa@gmail.com>
2023-07-14 20:38:26 +0300

    fix:correct requete hard

--- requestHard/Hard.sql
+++ requestHard/Hard.sql
@@ -1,5 +1,5 @@
 -- Afficher la liste des chambres libres sur une certaine période --
-SELECT romm.* NOT IN(SELECT room.* FROM romm INNER JOIN reservation 
-ON romm.id_reservation = reservation.id_reservation
+SELECT room.* NOT IN(SELECT room.* FROM room INNER JOIN reservation 
+ON romm.id = reservation.id_room
 WHERE date_donne>=date_arrived AND date_donne<=leaving_date); 
 -- ###Ici certaine periode est donné par l'utilisateur =>date_donne### --
